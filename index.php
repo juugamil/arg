ff2ec17d1df522df4118b48c28aac5a51d103a56
Juugamil <steezprm@gmail.com>
2021-11-24 10:21:32 -0500

    updated favicon

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ if(isset($_POST['submit'])){
         type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="/img/Logo.png">
     <script src="https://code.jquery.com/jquery-3.6.0.js"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" async defer></script>
     <script src="/js/mobilenav.js" async defer></script>
